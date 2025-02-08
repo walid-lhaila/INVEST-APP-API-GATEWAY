c7a76d93dcf291b28ae399365c7439d0877a9e9c
walid lhaila <walidlhaila00@gmail.com>
2025-02-08 17:48:44 +0100

    Feat: ADD NEW METHODE TO CREATE PROJECTS

--- src/Projects/projects.controller.ts
+++ src/Projects/projects.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Inject,
+  Post,
+  Headers,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('projects')
@@ -6,4 +13,19 @@ export class ProjectsController {
   constructor(
     @Inject('Projects-Service') private readonly projectsService: ClientProxy,
   ) {}
+
+  @Post('create')
+  async createProject(
+    @Body() projectsDto: any,
+    @Headers('authorization') token: string,
+  ) {
+    if (!token) {
+      throw new UnauthorizedException('TOKEN IS REQUIRED');
+    }
+    const payload = {
+      token,
+      payload: projectsDto,
+    };
+    return this.projectsService.send({ cmd: 'createProjects' }, payload);
+  }
 }
