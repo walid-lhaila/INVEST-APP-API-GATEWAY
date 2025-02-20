62178812b5e029bd00401fc5c0d70f62cfc06121
walid lhaila <walidlhaila00@gmail.com>
2025-02-20 21:36:59 +0100

    Feat: HANDLE COMMUNICATION BETWEEN MESSAGES SERVICES AND API-GATEWAY

--- src/Messages/messages.controller.ts
+++ src/Messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  Inject,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('messages')
@@ -7,4 +14,31 @@ export class MessagesController {
     @Inject('Messages-Service') private readonly messagesService: ClientProxy,
   ) {}
 
+  @Post('send')
+  async sendMessage(
+    @Headers('authorization') authHeader: string,
+    @Body('receiverId') receiverId: string,
+    @Body('content') content: string,
+  ) {
+    if (!authHeader) {
+      throw new UnauthorizedException('TOKEN IS REQUIRED');
+    }
+    const jwtToken = authHeader.split(' ')[1];
+    const decodedToken = JSON.parse(
+      Buffer.from(jwtToken.split('.')[1], 'base64').toString(),
+    );
+    const senderId = decodedToken?.sub;
+
+    if (!senderId) {
+      throw new UnauthorizedException('Invalid token');
+    }
+    const response = await this.messagesService
+      .send('send-message', {
+        senderId,
+        receiverId,
+        content,
+      })
+      .toPromise();
+    return { message: 'Message sent successfully', conversation: response };
+  }
 }
